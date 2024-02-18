8cac2d8a13f1f115b178bf0e7f418d83e8da6383
Peter <peterhamblin@hotmail.co.uk>
2024-02-18 21:36:07 +0000

    Program computes dimensional weight of a box

--- C_Practice/C Main.c
+++ C_Practice/C Main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+// Computes the dimensional weight of a 12"x10"x8" box
+
 int main(void)
 {
-    printf("Hello, World!\n");
+    const int HEIGHT = 8;
+    const int LENGTH = 12;
+    const int WIDTH = 10;
+    int volume = HEIGHT * LENGTH * WIDTH;
+    int weight = (volume + 165) / 166;
+
+    printf("Dimensions: %dx%dx%d\n", LENGTH, WIDTH, HEIGHT);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
 
     return 0;
 }
