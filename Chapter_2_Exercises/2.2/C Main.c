7af8a09a8d54df441f95c7dbe991db685dcb8cf8
Peter <peterhamblin@hotmail.co.uk>
2024-02-20 19:55:24 +0000

    Project completed

--- Chapter_2_Exercises/2.2/C Main.c
+++ Chapter_2_Exercises/2.2/C Main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
-
+    float volume = (4.0f / 3.0f) * M_PI * 10 * 10 * 10;
+    printf("%.2f", volume);
     return 0;
 }
