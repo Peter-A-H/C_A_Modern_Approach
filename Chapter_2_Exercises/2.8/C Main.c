e51b035e59c17b926e0c2e441c474d5847ac27b2
Peter <peterhamblin@hotmail.co.uk>
2024-02-21 13:44:18 +0000

    Project completed

--- Chapter_2_Exercises/2.8/C Main.c
+++ Chapter_2_Exercises/2.8/C Main.c
@@ -2,7 +2,26 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("Enter amount of loan: ");
+    float loan = 0.0f;
+    (void)scanf("%f", &loan);
+
+    printf("Enter interest rate: ");
+    float rate = 0.0f;
+    (void)scanf("%f", &rate);
+
+    printf("Enter monthly payment: ");
+    float payment = 0.0f;
+    (void)scanf("%f", &payment);
+
+    loan = loan - payment + (loan * rate / 100.0f / 12.0f);
+    printf("Balance remaining after first payment: " "\x9C%.2f\n", loan);
+
+    loan = loan - payment + (loan * rate / 100.0f / 12.0f);
+    printf("Balance remaining after second payment: " "\x9C%.2f\n", loan);
+
+    loan = loan - payment + (loan * rate / 100.0f / 12.0f);
+    printf("Balance remaining after third payment: " "\x9C%.2f\n", loan);
 
     return 0;
 }
