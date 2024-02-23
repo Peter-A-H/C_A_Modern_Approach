a09012af33fc46457112a64b55b2aaf37fc5a838
Peter <peterhamblin@hotmail.co.uk>
2024-02-23 21:29:05 +0000

    Completed project

--- Chapter_2_Exercises/3.1/C Main.c
+++ Chapter_2_Exercises/3.1/C Main.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("Enter a date (mm/dd/yyyy): ");
+    int month = 0;
+    int day = 0;
+    int year = 0;
+    (void)scanf("%d/%d/%d", &month, &day, &year);
+    printf("You entered the date %d/%2.2d/%d", year, month, day);
 
     return 0;
 }
