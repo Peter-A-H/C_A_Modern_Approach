fb2f1bedca6225a27e3aa57caf057d123ccc6afd
Peter <peterhamblin@hotmail.co.uk>
2024-02-24 22:22:09 +0000

    Initial commit

---
+++ Chapter_3_Projects/3.5/C Main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Enter the numbers from 1 to 16 in any order: ");
+    int n1 = 0;
+    int n2 = 0;
+    int n3 = 0;
+    int n4 = 0;
+    int n5 = 0;
+    int n6 = 0;
+    int n7 = 0;
+    int n8 = 0;
+    int n9 = 0;
+    int n10 = 0;
+    int n11 = 0;
+    int n12 = 0;
+    int n13 = 0;
+    int n14 = 0;
+    int n15 = 0;
+    int n16 = 0;
+    (void)scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8, &n9, &n10, &n11, &n12, &n13, &n14, &n15, &n16);
+
+    printf("");
+
+    return 0;
+}
