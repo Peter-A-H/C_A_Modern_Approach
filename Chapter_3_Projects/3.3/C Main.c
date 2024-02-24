f10d0c4b69d661f51aac74c9fc8c6b1816cfaab0
Peter <peterhamblin@hotmail.co.uk>
2024-02-24 21:20:17 +0000

    Project completed

--- Chapter_3_Projects/3.3/C Main.c
+++ Chapter_3_Projects/3.3/C Main.c
@@ -2,7 +2,19 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("Enter ISBN: ");
+    int gs1Prefix = 0;
+    int groupIdentifier = 0;
+    int publisherCode = 0;
+    int itemNumber = 0;
+    int checkDigit = 0;
+    (void)scanf("%d-%d-%d-%d-%d", &gs1Prefix, &groupIdentifier, &publisherCode, &itemNumber, &checkDigit);
+
+    printf("GS1 Prefix: %d\n", gs1Prefix);
+    printf("Group identifier: %d\n", groupIdentifier);
+    printf("Publisher code: %d\n", publisherCode);
+    printf("Item number: %d\n", itemNumber);
+    printf("Check digit: %d\n", checkDigit);
 
     return 0;
 }
