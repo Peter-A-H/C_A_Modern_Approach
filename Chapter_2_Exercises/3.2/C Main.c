642b52f940e432a066a9fd47ee0b7ce4942fc1e2
Peter <peterhamblin@hotmail.co.uk>
2024-02-24 20:48:58 +0000

    Project completed

--- Chapter_2_Exercises/3.2/C Main.c
+++ Chapter_2_Exercises/3.2/C Main.c
@@ -2,12 +2,23 @@
 
 int main(void)
 {
-    printf("Enter a date (mm/dd/yyyy): ");
+    printf("Enter item number: ");
+    int itemNumber = 0;
+    (void)scanf("%d", &itemNumber);
+
+    printf("Enter unit price: ");
+    float unitPrice = 0.0f;
+    (void)scanf("%f", &unitPrice);
+
+    printf("Enter purchase date (mm/dd/yyyy): ");
     int month = 0;
     int day = 0;
     int year = 0;
     (void)scanf("%d/%d/%d", &month, &day, &year);
-    printf("You entered the date %d/%2.2d/%d", year, month, day);
+
+    printf("Item\t\tUnit\t\tPurchase\n");
+    printf("\t\tPrice\t\tDate\n");
+    printf("%d\t\t$  %.2f\t%d/%d/%d", itemNumber, unitPrice, month, day, year);
 
     return 0;
 }
