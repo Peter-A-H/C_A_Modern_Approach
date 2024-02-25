e55f55b584ac5a4b7a9215ae3934cf0b6fbf5c3b
Peter <peterhamblin@hotmail.co.uk>
2024-02-25 17:03:42 +0000

    Project completed

--- Chapter_3_Projects/3.5/C Main.c
+++ Chapter_3_Projects/3.5/C Main.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     printf("Enter the numbers from 1 to 16 in any order: ");
+
     int n1 = 0;
     int n2 = 0;
     int n3 = 0;
@@ -21,7 +22,9 @@ int main(void)
     int n16 = 0;
     (void)scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8, &n9, &n10, &n11, &n12, &n13, &n14, &n15, &n16);
 
-    printf("");
+    printf("Row sums: %d %d %d %d\n", n1 + n2 + n3 + n4, n5 + n6 + n7 + n8, n9 + n10 + n11 + n12, n13 + n14 + n15 + n16);
+    printf("Column sums: %d %d %d %d\n", n1 + n5 + n9 + n13, n2 + n6 + n10 + n14, n3 + n7 + n11 + n15, n4 + n8 + n12 + n16);
+    printf("Diagonal sums: %d %d\n", n1 + n6 + n11 + n16, n4 + n7 + n10 + n13);
 
     return 0;
 }
