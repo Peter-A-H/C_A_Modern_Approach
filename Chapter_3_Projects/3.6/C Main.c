84036e1bb7ef00fb7732e6aa1e6ad5d2d4ffe0e2
Peter <peterhamblin@hotmail.co.uk>
2024-02-25 17:20:55 +0000

    Project completed

--- Chapter_3_Projects/3.6/C Main.c
+++ Chapter_3_Projects/3.6/C Main.c
@@ -2,7 +2,16 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("Enter fractions to add (a/b+x/y): ");
+    int num1 = 0;
+    int num2 = 0;
+    int denom1 = 0;
+    int denom2 = 0;
+    (void)scanf("%d/%d+%d/%d", &num1, &denom1, &num2, &denom2);
+
+    int resultNum = num1 * denom2 + num2 * denom1;
+    int resultDom = denom1 * denom2;
+    printf("The sum is %d/%d\n", resultNum, resultDom);
 
     return 0;
 }
