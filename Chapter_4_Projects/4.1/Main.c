0a54dbd6baf86879840e08add1a35a198fd6ab94
Peter <peterhamblin@hotmail.co.uk>
2024-02-26 20:27:50 +0000

    Check digit calculator

---
+++ Chapter_4_Projects/4.1/Main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Enter the first (single) digit: ");
+    int d = 0;
+    (void)scanf("%1d", &d);
+
+    printf("Enter the first group of five digits: ");
+    int a1 = 0;
+    int a2 = 0;
+    int a3 = 0;
+    int a4 = 0;
+    int a5 = 0;
+    (void)scanf("%1d%1d%1d%1d%1d", &a1, &a2, &a3, &a4, &a5);
+
+    printf("Enter the second group of five digits: ");
+    int b1 = 0;
+    int b2 = 0;
+    int b3 = 0;
+    int b4 = 0;
+    int b5 = 0;
+    (void)scanf("%1d%1d%1d%1d%1d", &b1, &b2, &b3, &b4, &b5);
+
+    int firstSum = d + a2 + a4 + b1 + b3 + b5;
+    int secondSum = a1 + a3 + a5 + b2 + b4;
+    int total = 3 * firstSum + secondSum;
+
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+    return 0;
+}
