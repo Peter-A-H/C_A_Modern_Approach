393d1ce35a4908ce74a8ead9558eee12038e90d8
Peter <peterhamblin@hotmail.co.uk>
2024-02-28 18:52:21 +0000

    Project completed

--- Chapter_4_Projects/4.5/Main.c
+++ Chapter_4_Projects/4.5/Main.c
@@ -2,25 +2,19 @@
 
 int main(void)
 {
-    printf("Enter the first (single) digit: ");
+    printf("Enter the 11 digit UPC code: ");
     int d = 0;
-    (void)scanf("%1d", &d);
-
-    printf("Enter the first group of five digits: ");
     int a1 = 0;
     int a2 = 0;
     int a3 = 0;
     int a4 = 0;
     int a5 = 0;
-    (void)scanf("%1d%1d%1d%1d%1d", &a1, &a2, &a3, &a4, &a5);
-
-    printf("Enter the second group of five digits: ");
     int b1 = 0;
     int b2 = 0;
     int b3 = 0;
     int b4 = 0;
     int b5 = 0;
-    (void)scanf("%1d%1d%1d%1d%1d", &b1, &b2, &b3, &b4, &b5);
+    (void)scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &a1, &a2, &a3, &a4, &a5, &b1, &b2, &b3, &b4, &b5);
 
     int firstSum = d + a2 + a4 + b1 + b3 + b5;
     int secondSum = a1 + a3 + a5 + b2 + b4;
